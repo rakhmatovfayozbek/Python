a1a4564a4113263f27eee8e770cdf25b1a33d756
Fayozbek Rakhmatov <117255567+rakhmatovfayozbek@users.noreply.github.com>
2022-11-06 13:55:20 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+# calculator v1
+
+what = input( "+ -" )
+
+a = float( input())
+b = float( input())
+
+if what == "+": 
+          c = a + b
+          print("=" + c)
+
+elif what == "-":
+          c = a - b
+          print("=" + c)
+
+else:
+          print( "Error" )
+
+input()
